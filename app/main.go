ee9a1a8fc19d5aef92a8190d6062b1dece58f450
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2025-11-14 19:22:19 +0530

    add echo

--- app/main.go
+++ app/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -14,17 +17,34 @@ func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		var cmd string
-		var subcmd string
-		fmt.Scanln(&cmd, &subcmd)
-		if cmd == "exit" {
-			v, err := strconv.Atoi(subcmd)
+		b, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
+		if err != nil {
+			log.Fatal("error reading input")
+		}
+		cmd := string(b)
+		splittedCmd := strings.Split(cmd, " ")
+		mainCmd := splittedCmd[0]
+		switch mainCmd {
+		case "exit":
+			subCmd := "0"
+			if len(cmd) == 2 {
+				subCmd = splittedCmd[1]
+			}
+			v, err := strconv.Atoi(subCmd)
 			if err != nil {
 				fmt.Fprint(os.Stdout, "invalid code")
 			}
 			os.Exit(v)
+
+		case "echo":
+			for _, val := range splittedCmd[1:] {
+				fmt.Print(val + " ")
+			}
+			fmt.Println()
+
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", splittedCmd[0])
 		}
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
 
 	}
 }
