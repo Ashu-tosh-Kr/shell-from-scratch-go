835fc8e3ca39b61e9688ac37db35582e3b0b650d
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2025-11-18 02:14:45 +0530

    implement basic redraw logic

--- app/readLine/readLine.go
+++ app/readLine/readLine.go
@@ -3,40 +3,68 @@ package readline
 import (
 	"fmt"
 	"io"
+	"unicode/utf8"
 )
 
 type ReadLine struct {
 	Stdin  io.ReadCloser
 	Stdout io.WriteCloser
 	Stderr io.WriteCloser
+	buf    []rune
+	cursor int
 }
 
 func NewReadLine(Stdin io.ReadCloser, Stdout io.WriteCloser, Stderr io.WriteCloser) ReadLine {
-	return ReadLine{Stdin: Stdin, Stdout: Stdout, Stderr: Stderr}
+	return ReadLine{Stdin: Stdin, Stdout: Stdout, Stderr: Stderr, buf: make([]rune, 4096), cursor: 2}
 }
 
 func (rl *ReadLine) Read() ([]byte, error) {
-	buf := make([]byte, 1024)
-	read := 0
+	byteBuf := make([]byte, 1024)
 	for {
-		n, _ := rl.Stdin.Read(buf[read:])
-		for i := read; i < read+n; i++ {
-			if buf[i] == 0x03 { // Ctrl-C
-				return make([]byte, 1), io.EOF
-
-			}
+		n, _ := rl.Stdin.Read(byteBuf)
+		runeBuf := make([]rune, 0)
+		for n > 0 {
+			r, size := utf8.DecodeRune(byteBuf[:n])
+			runeBuf = append(runeBuf, r)
+			copy(byteBuf, byteBuf[size:n])
+			n -= size
 		}
-		rl.Echo(buf[read : read+n])
+		// fmt.Println("a")
+		rl.handleInput(runeBuf)
 
-		read += n
 	}
 
 }
 
-func (rl *ReadLine) Echo(b []byte) {
-	fmt.Fprint(rl.Stdout, string(b))
+func (rl *ReadLine) handleInput(runeBuf []rune) ([]byte, error) {
+	for _, run := range runeBuf {
+		switch run {
+		case 0x03:
+			return make([]byte, 1), io.EOF
+		// case :
+		default:
+			// fmt.Println("b")
+			rl.buf = append(rl.buf, run)
+			rl.cursor++
+			rl.redrawLine()
+		}
+
+	}
+	// rl.echo(rl.buf)
+	return make([]byte, 2), nil
 }
 
-func parseInput() {
-
+func (rl *ReadLine) redrawLine() {
+	fmt.Fprint(rl.Stdout, "\r")
+	line := "$ " + string(rl.buf)
+	fmt.Print(line)
+	// fmt.Print("\x1b[K")
+	// wanted := len("$ ") + rl.cursor
+	// current := len(line)
+	// diff := current - wanted
+	// // fmt.Println("c")
+	// if diff > 0 {
+	// 	// Move left diff
+	// 	fmt.Printf("\x1b[%dD", diff)
+	// }
 }
